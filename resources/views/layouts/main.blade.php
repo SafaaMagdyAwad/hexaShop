5e658c67d01be893ea959a81a16f67ad927ea076
SafaaMagdyAwad <engsafaamagdy1024@gmail.com>
2024-10-07 18:25:39 +0300

    adding layouts&includes

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+@include('includes.head')
+
+<body>
+    @include('includes.preloader')
+    @yield('content')
+
+    @include('includes.footer')
+    @include('includes.script')
+</body>
+
+</html>
