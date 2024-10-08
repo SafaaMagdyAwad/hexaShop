f8f9da75d180c21805d1260e8d5a2669b12ee6d7
SafaaMagdyAwad <engsafaamagdy1024@gmail.com>
2024-10-08 11:36:53 +0300

    .

--- resources/views/includes/subscripe-area.blade.php
+++ resources/views/includes/subscripe-area.blade.php
@@ -1,73 +1,73 @@
   <!-- ***** Subscribe Area Starts ***** -->
   @if (session('success'))
-        <div class="alert alert-success">
-            {{ session('success') }}
-        </div>
-    @endif
-    @if (session('error'))
-        <div class="alert alert-danger">
-            {{ session('error') }}
-        </div>
-    @endif
+      <div class="alert alert-success">
+          {{ session('success') }}
+      </div>
+  @endif
+  @if (session('error'))
+      <div class="alert alert-danger">
+          {{ session('error') }}
+      </div>
+  @endif
   <div class="subscribe">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-8">
-                <div class="section-heading">
-                    <h2>By Subscribing To Our Newsletter You Can Get 30% Off</h2>
-                    <span>Details to details is what makes Hexashop different from the other themes.</span>
-                </div>
-                <form id="subscribe" action="{{ route('subscribe') }}" method="post">
-                    @csrf
-                    <div class="row">
-                        <div class="col-lg-5">
-                            @error('name')
-                            <div class="alert alert-danger">{{ $message }}</div>
-                        @enderror
-                            <fieldset>
-                                <input name="name" type="text" id="name" placeholder="Your Name"
-                                    required="">
-                            </fieldset>
-                        </div>
-                        <div class="col-lg-5">
-                            @error('email')
-                            <div class="alert alert-danger">{{ $message }}</div>
-                        @enderror
-                            <fieldset>
-                                <input name="email" type="text" id="email" pattern="[^ @]*@[^ @]*"
-                                    placeholder="Your Email Address" required="">
-                            </fieldset>
-                        </div>
-                        <div class="col-lg-2">
-                            <fieldset>
-                                <button type="submit" id="form-submit" class="main-dark-button"><i
-                                        class="fa fa-paper-plane"></i></button>
-                            </fieldset>
-                        </div>
-                    </div>
-                </form>
-            </div>
-            <div class="col-lg-4">
-                <div class="row">
-                    <div class="col-6">
-                        <ul>
-                            <li>Store Location:<br><span>Sunny Isles Beach, FL 33160, United States</span></li>
-                            <li>Phone:<br><span>010-020-0340</span></li>
-                            <li>Office Location:<br><span>North Miami Beach</span></li>
-                        </ul>
-                    </div>
-                    <div class="col-6">
-                        <ul>
-                            <li>Work Hours:<br><span>07:30 AM - 9:30 PM Daily</span></li>
-                            <li>Email:<br><span>info@company.com</span></li>
-                            <li>Social Media:<br><span><a href="#">Facebook</a>, <a
-                                        href="#">Instagram</a>, <a href="#">Behance</a>, <a
-                                        href="#">Linkedin</a></span></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- ***** Subscribe Area Ends ***** -->
+      <div class="container">
+          <div class="row">
+              <div class="col-lg-8">
+                  <div class="section-heading">
+                      <h2>By Subscribing To Our Newsletter You Can Get 30% Off</h2>
+                      <span>Details to details is what makes Hexashop different from the other themes.</span>
+                  </div>
+                  <form id="subscribe" action="{{ route('subscribe') }}" method="post">
+                      @csrf
+                      <div class="row">
+                          <div class="col-lg-5">
+                              @error('name')
+                                  <div class="alert alert-danger">{{ $message }}</div>
+                              @enderror
+                              <fieldset>
+                                  <input name="name" type="text" id="name" placeholder="Your Name"
+                                      required="">
+                              </fieldset>
+                          </div>
+                          <div class="col-lg-5">
+                              @error('email')
+                                  <div class="alert alert-danger">{{ $message }}</div>
+                              @enderror
+                              <fieldset>
+                                  <input name="email" type="text" id="email" pattern="[^ @]*@[^ @]*"
+                                      placeholder="Your Email Address" required="">
+                              </fieldset>
+                          </div>
+                          <div class="col-lg-2">
+                              <fieldset>
+                                  <button type="submit" id="form-submit" class="main-dark-button"><i
+                                          class="fa fa-paper-plane"></i></button>
+                              </fieldset>
+                          </div>
+                      </div>
+                  </form>
+              </div>
+              <div class="col-lg-4">
+                  <div class="row">
+                      <div class="col-6">
+                          <ul>
+                              <li>Store Location:<br><span>Sunny Isles Beach, FL 33160, United States</span></li>
+                              <li>Phone:<br><span>010-020-0340</span></li>
+                              <li>Office Location:<br><span>North Miami Beach</span></li>
+                          </ul>
+                      </div>
+                      <div class="col-6">
+                          <ul>
+                              <li>Work Hours:<br><span>07:30 AM - 9:30 PM Daily</span></li>
+                              <li>Email:<br><span>info@company.com</span></li>
+                              <li>Social Media:<br><span><a href="#">Facebook</a>, <a
+                                          href="#">Instagram</a>, <a href="#">Behance</a>, <a
+                                          href="#">Linkedin</a></span></li>
+                          </ul>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+  <!-- ***** Subscribe Area Ends ***** -->
